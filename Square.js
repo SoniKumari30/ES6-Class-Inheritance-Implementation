0e295f10850fbb78c7f1f9429aa083fcc5434ba2
soni kumari <sonikumarisharma30@gmail.com>
2021-05-22 02:02:48 +0530

    first commit

---
+++ Square.js
@@ -0,0 +1,19 @@
+class Square extends Circle {
+    constructor(x, y) {
+      super(x, y);
+      this.bright = random(255);
+      this.r = 10;
+    }
+  
+    update() {
+      super.update();
+      this.r += random(-2, 2);
+    }
+  
+    show() {
+      strokeWeight(1);
+      stroke(255);
+      fill(this.bright);
+      square(this.x, this.y, this.r);
+    }
+  }
